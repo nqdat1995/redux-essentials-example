33a23b3ae5c3108631fccefa632ea575be33de00
datnq29692 <datnq29692@sacombank.com>
2021-11-10 13:24:53 +0700

    Configure secure headers and static buildpacks

--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+import { fetchNotifications } from '../features/notifications/notificationSlice'
 
 export const Navbar = () => {
+  const dispatch = useDispatch()
+
+  const fetchNewNotification = () => {
+    dispatch(fetchNotifications())
+  }
+
   return (
     <nav>
       <section>
         <h1>Redux Essentials Example</h1>
 
         <div className="navContent">
-          <div className="navLinks"></div>
+          <div className="navLinks">
+            <Link to="/">Posts</Link>
+            <Link to="/users">Users</Link>
+            <Link to="/notifications">Notifications</Link>
+          </div>
         </div>
+        <button className="button" onClick={fetchNewNotification}>
+          Refresh Notifications
+        </button>
       </section>
     </nav>
   )
--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import postReducer from '../features/posts/postSlice'
+import userReducer from '../features/users/usersSlice'
+import notificationReducer from '../features/notifications/notificationSlice'
 
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    posts: postReducer,
+    users: userReducer,
+    notifications: notificationReducer,
+  },
 })
